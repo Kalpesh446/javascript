060cf562c1e209a1d60b6c4294a70fab8e8904a7
Kalpesh446 <121086899+Kalpesh446@users.noreply.github.com>
2023-09-27 17:54:53 +0530

    program to find the factorial of a number

---
+++ javascript practicals/03_Factrorial_Number.js
@@ -0,0 +1,21 @@
+// program to find the factorial of a number
+
+// take input from the user
+const number = 2;
+
+// checking if number is negative
+if (number < 0) {
+  console.log("Error! Factorial for negative number does not exist.");
+}
+//   if number is 0
+else if (number == 0) {
+  console.log(`the factorial of ${number} is 1.`);
+}
+// if number is positive
+else {
+  let fact = 1;
+  for (let i = 1; i <= number; i++) {
+    fact *= i;
+  }
+  console.log(`the factorial of ${number} is ${fact}`);
+}
