4f1d42305f3f79f81849bbe0f32dd9ff9992024a
Kalpesh446 <121086899+Kalpesh446@users.noreply.github.com>
2023-10-23 10:58:24 +0530

    array

---
+++ javascript practice/1_Array.js
@@ -0,0 +1,11 @@
+let books = ["math", "gujrati", "bio", "social"];
+
+let bookwithPages = [
+  ["math", "100"],
+  ["gujarti", "200"],
+  ["bio", "180"],
+  ["social", "210"],
+];
+
+let fetch = bookwithPages[2];
+console.log(fetch);
