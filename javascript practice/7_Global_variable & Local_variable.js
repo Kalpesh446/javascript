eedd57c56641b7e2a894c836df2584e4d3d1e889
Kalpesh Chavada <121086899+Kalpesh446@users.noreply.github.com>
2023-10-24 15:26:08 +0530

    global vs local

---
+++ javascript practice/7_Global_variable & Local_variable.js
@@ -0,0 +1,14 @@
+// Globalvariable & Localvariable
+
+let car = "Audi"; //Global variable
+
+function add() {
+  let result = "pass"; // Local variable
+  console.log(result);
+}
+add();
+
+function name() {
+  console.log(car);
+}
+name();
