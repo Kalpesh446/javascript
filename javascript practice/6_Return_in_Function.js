7819d69ceb6d080cb0791ed98a614f63b0227a9e
Kalpesh Chavada <121086899+Kalpesh446@users.noreply.github.com>
2023-10-24 15:25:40 +0530

    return in function

---
+++ javascript practice/6_Return_in_Function.js
@@ -0,0 +1,21 @@
+// function add(a, b) {
+//   let d = [3, 4, 5];
+//   return d;
+// }
+
+// let c = add(4, 3);
+
+// console.log(c);
+
+function compare(a, b) {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+let c = compare(7, 7);
+console.log(c);
