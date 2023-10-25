32124d695fb3fb705ab0cd0c1333496b51d8eaee
Kalpesh Chavada <121086899+Kalpesh446@users.noreply.github.com>
2023-10-25 11:38:39 +0530

    New keyword for creating new object

---
+++ javascript practice/13_New_Keyword.js
@@ -0,0 +1,11 @@
+// let person = {
+//   Name: "kalpesh",
+//   age: 24,
+// };
+
+let person = new Object();
+person.Name = "kalpesh";
+person.age = 29;
+person.lastname = "chavada";
+
+console.log(person.lastname);
