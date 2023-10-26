10d4a7890cd7d0db571d9fec5665bc7308fff71b
Kalpesh Chavada <121086899+Kalpesh446@users.noreply.github.com>
2023-10-26 10:41:18 +0530

    Hoisting with function and variable

---
+++ javascript practice/17_Hoisting.js
@@ -0,0 +1,24 @@
+// hoisting with function
+
+// hello();
+
+// function hello() {
+//   console.log("hoisting");
+// }
+
+// hoisting with variable with var
+// console.log(x);
+// var x;
+
+// x = 40;
+
+// let and const
+// console.log(z);
+// let z;
+// z = 30;
+
+"use strict";
+
+y = 100;
+
+console.log(y);
