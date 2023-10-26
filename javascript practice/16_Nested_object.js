39f196d1d37cea7f5cd15870b4cfe12787478039
Kalpesh Chavada <121086899+Kalpesh446@users.noreply.github.com>
2023-10-26 10:40:13 +0530

    Nested object

---
+++ javascript practice/16_Nested_object.js
@@ -0,0 +1,15 @@
+let nested = {
+  name: "kalp",
+  last: "kkaka",
+  age: 10,
+  add: {
+    line1: "hjsdhdh",
+    street: "surat",
+    city: "rajkot",
+    per: {
+      sex: "fr",
+      lal: "jjs",
+    },
+  },
+};
+console.log(nested.add.per.sex);
