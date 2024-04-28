5d74f1ce8d7217f10c20cc2ecd7b8a181f5391f3
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-04-28 20:14:13 +0530

    change

--- javascript practicals/32_For loop.js
+++ javascript practicals/32_For loop.js
@@ -10,5 +10,5 @@ for (let i = 0; i > 5; i++) {
 
 // what is print in this console.log
 for (let i = 0; i <= 15; i++) {
-  console.log(i++); //0, 2, 4, 6, 8, 10, 12, 14;
+  console.log(i++); // 0, 2, 4, 6, 8, 10, 12, 14;
 }
