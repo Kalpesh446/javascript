272eb097364f344be27bb204e0eff5d77afc49c2
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-04-28 20:07:32 +0530

    interview for loop question

---
+++ javascript practicals/32_For loop.js
@@ -0,0 +1,14 @@
+// what is print in this console.log
+for (let i = 0; i < 5; i++) {
+  console.log(i); // 1,2,3,4
+}
+
+// what is print in this console.log
+for (let i = 0; i > 5; i++) {
+  console.log(i); // undefined
+}
+
+// what is print in this console.log
+for (let i = 0; i <= 15; i++) {
+  console.log(i++); //0, 2, 4, 6, 8, 10, 12, 14;
+}
