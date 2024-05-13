f7a5f7094edcb4f0f4ecd3f28523c95ebf98fcb4
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-05-13 21:27:05 +0530

    closures in js with examples

---
+++ javascript practice/63_What are Closures in JavaScript.js
@@ -0,0 +1,17 @@
+// Closures provide a better, and concise way of writing JavaScript code for the developers and programmers. Closures are created whenever a variable that is defined outside the current scope is accessed within the current scope.
+
+function hello(name) {
+  var message = "hello " + name;
+
+  return function hello() {
+    console.log(message);
+  };
+}
+
+//generate closure
+
+var helloWorld = hello("World");
+
+//use closure
+
+helloWorld();
