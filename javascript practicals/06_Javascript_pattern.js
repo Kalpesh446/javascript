e2ed03f4e7e677e5db2fbf1f0e10659898347408
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-06-16 17:28:38 +0530

    pattern

--- javascript practicals/06_Javascript_pattern.js
+++ javascript practicals/06_Javascript_pattern.js
@@ -1,54 +1,107 @@
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
 for (let i = 1; i <= 5; i++) {
-  for (let j = 1; j <= i; j++) {
-    document.write(i);
+  for (let j = 0; j < i; j++) {
+    document.writeln(i + " ");
   }
-  document.write("<br>");
+  document.writeln("<br>");
 }
+document.writeln("<br>");
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
 
 for (let i = 1; i <= 5; i++) {
   for (let j = 1; j <= i; j++) {
-    document.write(j);
+    document.write(j + " ");
   }
-  document.write("<br>");
+  document.writeln("<br>");
 }
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
 
 let count = 1;
-for (let i = 0; i <= 5; i++) {
-  for (let j = 0; j <= i; j++) {
-    document.write(count + " ");
-    count++;
+for (let i = 1; i <= 6; i++) {
+  for (let j = 1; j < i; j++) {
+    document.write(count++ + " ");
   }
-  document.write("<br>");
+  document.writeln("<br>");
 }
+
+//     1
+//    2 2
+//   3 3 3
+//  4 4 4 4
+// 5 5 5 5 5
+
+document.write("<br>");
 
 for (let i = 1; i <= 5; i++) {
   for (let j = 5; j >= i; j--) {
-    document.write("&nbsp;");
+    document.write("&nbsp");
   }
   for (let k = 1; k <= i; k++) {
     document.write(i + " ");
   }
-  document.write("<br>");
+  document.writeln("<br>");
 }
+
+//     1
+//    1 2
+//   1 2 3
+//  1 2 3 4
+// 1 2 3 4 5
 
 for (let i = 1; i <= 5; i++) {
   for (let j = 5; j >= i; j--) {
-    document.write("&nbsp;");
+    document.writeln("&nbsp");
   }
   for (let k = 1; k <= i; k++) {
-    document.write(k + " ");
+    document.writeln(k + " ");
   }
-  document.write("<br>");
+  document.writeln("<br>");
+}
+document.writeln("<br>");
+//     1
+//    1 2
+//   1 2 3
+//  1 2 3 4
+// 1 2 3 4 5
+let count_1 = 1;
+for (let i = 0; i <= 5; i++) {
+  for (let j = 5; j >= i; j--) {
+    document.writeln("&nbsp;");
+  }
+  for (let k = 1; k <= i; k++) {
+    document.writeln(count_1 + " ");
+    count_1++;
+  }
+  document.writeln("<br>");
 }
 
-let count1 = 1;
-for (let i = 1; i <= 5; i++) {
+//     1
+//    3 5
+//   7 9 11
+//  13 15 17 19
+// 21 23 25 27 29
+
+let count_2 = 1;
+for (let i = 0; i <= 5; i++) {
   for (let j = 5; j >= i; j--) {
-    document.write("&nbsp;");
+    document.writeln("&nbsp;");
   }
   for (let k = 1; k <= i; k++) {
-    document.write(count1 + " ");
-    count1++;
+    document.writeln(count_2 + " ");
+    count_2 += 2;
   }
-  document.write("<br>");
+  document.writeln("<br>");
 }
